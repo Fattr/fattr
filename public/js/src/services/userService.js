3319eff3b0eb552d7b9bc8825681c0e463c11dc3
Wayne Montague <wayne.montague@gmail.com>
2014-01-22 16:19:05 -0800

    experimenting with general color scheme

--- public/js/src/services/userService.js
+++ public/js/src/services/userService.js
@@ -2,28 +2,16 @@ angular.module('fittr.services')
 
 .provider('UserService', function() {
   var baseUrl = "http://localhost:3000/";
-  // var apiKey = '';
-
-  // // Set our API key from the .config section
-  // // of our app
-  // this.setApiKey = function(key) {
-  //   apiKey = key || apiKey;
-  // };
-
 
   this.$get = function($http, $q, localStorageService) {
     return {
       // persist user details in memory
       currentUser: {},
-      // TODO: integrate with user object.  quick and dirty hack :/
-      // sessionToken: '',
 
       signup: function(user) {
         console.log(user);
         var creatingUser = $q.defer();
         
-        // configure http to send app access token along with POST
-        // $http.defaults.headers.common['fittr-api-key'] = apiKey;
         $http.post(baseUrl + 'signup', user)
           .success(function(data, status, headers, config) {
             console.log("data: ", data, "status: ", status);
@@ -31,8 +19,6 @@ angular.module('fittr.services')
           })
           .error(function(data, status, headers, config) {
             creatingUser.reject(data, status);
-          // TODO: investigate how to indicate to user that signup was successfull
-          // TODO: investigate how to properly move from this state to connect devices state
           });
 
         return creatingUser.promise;
@@ -66,10 +52,9 @@ angular.module('fittr.services')
 
       retrieve: function(userId, token) {
         var retrievingUser = $q.defer();
-        // $http.defaults.headers.common['fittr-api-key'] = apiKey;
-        // $http.defaults.headers.common['fittr-session-token'] = token;
+
         console.log("retrieve: ", userId);
-        $http.get(baseUrl + "/" + userId)
+        $http.get(baseUrl + "users/" + userId)
           .success(function(data, status, headers, config) {
             console.log("data: ", data, "status: ", status);
             retrievingUser.resolve(data);     
@@ -84,16 +69,12 @@ angular.module('fittr.services')
         this.currentUser = userData;
       },
 
-      setSessionToken: function(token) {
-        this.sessionToken = token;
-      },
-
       saveToLocal: function(userData) {
         console.log("saving user into localStorage");
         localStorageService.add('currentUser', userData);
       },
       retrieveFromLocal: function() {
-        localStorageService.get('currentUser');
+        return localStorageService.get('currentUser');
       }
     };
   };
