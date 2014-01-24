7ca3679497fadfc5976543d48a15772925360459
Wayne Montague <wayne.montague@gmail.com>
2014-01-24 12:20:00 -0800

    adds title bar to signup-login template (closes #40).  adds error flash message to signup view.

--- public/js/src/controllers/signupController.js
+++ public/js/src/controllers/signupController.js
@@ -3,6 +3,8 @@ angular.module('fittr.controllers')
   .controller('SignupController', function($scope, $http, UserService, $state) {
     $scope.title = "Sign Up";
     $scope.user = {};
+    $scope.signupLoginError = false;
+    $scope.flashMessage = "";
 
     var resetForm = function(ngFormController) {
       $scope.user.email = "";
@@ -34,21 +36,33 @@ angular.module('fittr.controllers')
         $scope.signupLoginForm.$valid;
     };
 
+    $scope.dismiss = function() {
+      $scope.signupLoginError = false;
+    }
+
     $scope.submit = function(ngFormController) {
       $scope.user.username = $scope.user.email;
       var signupPromise = UserService.signup($scope.user);
 
       signupPromise.then(function(data) {
-          console.log("signup: ", data);
 
+          // console.log("signup: ", data);
           resetForm(ngFormController); 
+
           // move to connect devices state
           $state.go('connect-devices');
         }, function(reason) {
           resetForm(ngFormController);
           console.log("reason: ", reason);
+          // Display a flash message indicating error
+          // TODO: would be cool to send back to the user the 
+          // email address they used to sign up
+          $scope.flashMessage = 'Hmmm, looks like you already have an account.';  //TODO:
+          $scope.signupLoginError = true
         });
 
+      // Retrieve the newly created User data so that we can save in
+      // memory and in local storage
       signupPromise.then(function(data) {
         UserService.retrieve(data._id, data._access_token)
           .then(function(data) {
