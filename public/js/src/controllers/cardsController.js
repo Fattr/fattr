79cba5481af37e9de1c62eb60e56f458cf79e885
Wayne Montague <wayne.montague@gmail.com>
2014-01-23 16:27:57 -0800

    more changes

--- public/js/src/controllers/cardsController.js
+++ public/js/src/controllers/cardsController.js
@@ -1,28 +1,8 @@
 angular.module('fittr.controllers')
 
-.controller('CardsController', function($scope) {
-  $scope.users = [
-    {
-      name: 'David',
-      steps: 8785
-    },
-    {
-      name: 'Scott',
-      steps: 5255
-    },
-    {
-      name: 'Wayne',
-      steps: 7573
-    },
-    {
-      name: 'Santiago',
-      steps: 10083
-    },
-    {
-      name: 'Mehul',
-      steps: 15000
-    }
-  ];
+.controller('CardsController', function($scope, UserService) {
+  console.log("cards controller: ", UserService);
+  $scope.users = UserService.users;
 
   $scope.back = function() {
     
