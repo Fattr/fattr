60be18a66d6d160959840ced7378dc8c704430be
Wayne Montague <wayne.montague@gmail.com>
2014-01-23 16:48:23 -0800

    able to transition to main screen

--- public/js/src/controllers/cardsController.js
+++ public/js/src/controllers/cardsController.js
@@ -2,7 +2,15 @@ angular.module('fittr.controllers')
 
 .controller('CardsController', function($scope, UserService) {
   console.log("cards controller: ", UserService);
-  $scope.users = UserService.users;
+
+  UserService.getAllUsers()
+    .then(function(data) {
+      // reverse order of activities
+      console.log(data);
+      UserService.users = data;
+      console.log("grab all users: ", UserService.users);
+      $scope.users = UserService.users;
+    })
 
   $scope.back = function() {
     
