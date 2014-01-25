9e4bb9518279c67359c2d653114af6be314b221a
sudostack <dwu207@gmail.com>
2014-01-25 09:54:38 -0800

    more cleanup

--- public/js/src/services/directives.js
+++ public/js/src/services/directives.js
@@ -1,12 +1,6 @@
 angular.module('fittr')
 
 .directive('cardtop', ['$swipe', function($swipe, differential) {
-  // return function(scope, element, attrs) {
-  //   var fn = function() {
-  //     console.log('differential', differential);
-  //     console.log(element[0].getBoundingClientRect().top);
-  //   };
-  // };
 
   return {
     restrict: 'EA',
@@ -33,24 +27,7 @@ angular.module('fittr')
       });
     }
   };
-    // link: function(scope, element, attrs) {
-    //   var threshold = $window.innerHeight * 0.3;
-    //   scope.top = element[0].getBoundingClientRect().top;
-    //   console.log('offsetTop', attrs.$$element[0].offsetTop);
-
-    //   scope.$watch(attrs.$$element[0].offsetTop, function(newVal, oldVal) {
-    //     if (newVal < threshold) {
-    //       differential.currentCardUser = scope.user.username;
-    //       console.log(differential.currentCardUser);
-    //     }
-    //   }, true);
-
-      // setInterval(function() {
-      //   console.log(differential.currentCardUser);
-      // }, 2500);
-    // }
-  // };
-}]);
+}])
 
 // directive watching the scroll event on the cardtop directive
 // this directive will be applied to topbar
