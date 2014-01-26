cffbd22b8d0e17d02d302a56a06dd391596c8e3a
sudostack <dwu207@gmail.com>
2014-01-25 16:03:08 -0800

    redirect logged-in users to /main/stream

--- public/js/src/app.js
+++ public/js/src/app.js
@@ -22,17 +22,37 @@ angular.module('fittr', ['ionic', 'ngRoute', 'LocalStorageModule', 'nvd3ChartDir
   // Each state's controller can be found in controllers.js
 
     // ENTRY
+  var checkEntry = function($q, $state, $http, $rootScope) {
+    // check localStorage to see if user is already logged in
+    // if not continue on.
+
+    var user = null;
+
+    var deferred = $q.defer();
+    $http.get('/loggedin').success(function(user) {
+      if(user !== '0') {
+        deferred.resolve();
+        $state.go('main.stream');
+      } else {
+        deferred.resolve();
+      }
+    });
+    return deferred.promise;
+  };
+
+
   var checkAuth = function($q, $state, $http, $rootScope) {
     // check localStorage to see if user is already logged in
     // if not continue on.
 
-    var User = null;
+    var user = null;
     $rootScope.isAuth = function() {
       return user; // might come in handy to show and hide links based on if the user is auth
       // example is show sign in/ log in if no user is auth and hide logout
       // vice versa if user is logged in. Just do a ng-if='$rootScope.isAuth()'
       // if it doesn't work, then the User variable might need to be a $root variable
     };
+
     var deferred = $q.defer();
     // Promise for ajax call to check if user is logged in the sever
     // use on any state / template to prevent access
@@ -52,6 +72,7 @@ angular.module('fittr', ['ionic', 'ngRoute', 'LocalStorageModule', 'nvd3ChartDir
     return deferred.promise;
   };
 
+
   /*
    * Fittr
    */
@@ -60,6 +81,9 @@ angular.module('fittr', ['ionic', 'ngRoute', 'LocalStorageModule', 'nvd3ChartDir
     .state('entry', {
       url: '/',
       templateUrl: 'templates/entry.html',
+      resolve: {
+        loggedin: checkEntry
+      }
     })
 
     .state('signup', {
