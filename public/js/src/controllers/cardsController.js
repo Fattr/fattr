6f38c346f29edfcd8b72666d9ce908f7ec1cc049
Wayne Montague <wayne.montague@gmail.com>
2014-01-26 20:29:19 -0800

    fixed issue where topBar.html producing a weird 404 error.  turns out you can't have spaces within angulars binding braces.

--- public/js/src/controllers/cardsController.js
+++ public/js/src/controllers/cardsController.js
@@ -23,19 +23,6 @@ angular.module('fittr.controllers')
   ];
   // DUMMY DATA END
 
-  // Retrieve current user's activity for the past 7 days
-  // and store in memory and localStorage
-  // UserService.currentUser = UserService.getFromLocal();
-  UserService.loadCurrentUser();
-
-  UserService.getActivity(UserService.currentUser._id, 7)
-    .then(function(data) {
-      console.log("response from get activity: ", data);
-      UserService.saveActivity(UserService.currentUser._id, data);
-      $scope.currentUser = UserService.currentUser;
-    }, function(data) {
-      console.log("an error occured (get activity)");
-    });
       
   $scope.currentUser = UserService.currentUser;
   $scope.users = users;
--- public/js/src/controllers/topBarController.js
+++ public/js/src/controllers/topBarController.js
@@ -2,6 +2,17 @@ angular.module('fittr.controllers')
 
 .controller('TopBarController', function($scope, UserService) {
 
+  // Retrieve current user's activity for the past 7 days
+  // and store in memory and localStorage
+  UserService.getActivity(UserService.currentUser._id, 7)
+    .then(function(data) {
+      console.log("response from get activity: ", data);
+      UserService.saveActivity(UserService.currentUser._id, data);
+      $scope.currentUser = UserService.currentUser;
+    }, function(data) {
+      console.log("an error occured (get activity)");
+    });
+
   $scope.currentUser = UserService.currentUser;
   if ($scope.currentUser.authData && $scope.currentUser.authData.fitbit) {
     $scope.avatar = $scope.currentUser.authData.fitbit.avatar;
