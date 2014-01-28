1528cf1de6e7b6ab4b9ea2cac5f54c0cbf81fc04
sudostack <dwu207@gmail.com>
2014-01-28 15:30:25 -0800

    added more danglin' styles

--- public/js/src/controllers/cardsController.js
+++ public/js/src/controllers/cardsController.js
@@ -32,14 +32,13 @@ angular.module('fittr.controllers')
 
   // // Retrieve current user's activity for the past 7 days
   // // and store in memory and localStorage
-  UserService.currentUser = UserService.getFromLocal();
+  // UserService.currentUser = UserService.getFromLocal();
 
   UserService.getActivity(UserService.currentUser._id, 7)
     .then(function(data) {
       // console.log("response from get activity: ", data);
       // debugger;
       UserService.saveActivity(UserService.currentUser._id, data);
-      $scope.currentUser = UserService.currentUser;
     }, function(data) {
       console.log("an error occured (get activity)");
     });
