8c8fa8f02d36f6d74c40d56f5acfff07a278d1f1
Wayne Montague <wayne.montague@gmail.com>
2014-01-29 00:09:04 -0800

    minor cleanup

--- public/js/src/controllers/topBarController.js
+++ public/js/src/controllers/topBarController.js
@@ -1,23 +1,6 @@
 angular.module('fittr.controllers')
 
 .controller('TopBarController', function($q, $scope, UserService) {
-
-  /*
-   * USER ACTIVITY RETRIEVAL
-   */
-  // var gettingActs = $q.defer();
-  // UserService.getActivity(UserService.currentUser._id, 7)
-  //   .then(function(data) {
-  //     console.log("UserService.getActivity()", data);
-  //     // resolve promise
-  //     gettingActs.resolve(data);
-  //     // save to local storage
-  //     UserService.saveActivity(UserService.currentUser._id, data);
-  //     $scope.currentUser = data[0]; 
-  //   }, function(data) {
-  //     console.log("an error occured (get activity)");
-  //   });
-
   /*
    * USER AVATAR
    */
@@ -34,9 +17,6 @@ angular.module('fittr.controllers')
   } else {
     $scope.avatar = 'http://ionicframework.com/img/docs/mcfly.jpg';
   }
-
-  //$scope.me = UserService.currentUser;
-
 });
 
 
