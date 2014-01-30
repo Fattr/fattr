6720109f33223dc5e8fe76ecd0bacafff446dfc5
Santiago <santiago.archila@gmail.com>
2014-01-29 20:03:01 -0800

    fixes issue where user comparisons were not working after signup

--- public/js/src/controllers/cardsController.js
+++ public/js/src/controllers/cardsController.js
@@ -1,31 +1,37 @@
 angular.module('fittr.controllers')
 
 .controller('CardsController', function($q, $scope, UserService){
-
-
   $scope.Math = window.Math; // so that we can use Math operations in angular expressions
 
-  // var getUsers = $q.defer();
-  UserService.getAll(0)
-    .then(function(data) {
-      // console.log("getAll: ", data);
+  // GET your stats from yesterday
+  UserService.get().then(function(userData){
+    
+    // and save your activity to the UserService.currentUser
+    UserService.saveActivity(userData.stats._id, userData);
+    
+    // After your data is saved into UserService.currentUser
+    // we can get all the other users' activity stats from yesterday
+    UserService.getAll(0).then(function(data) {
 
       $scope.users = data;
       UserService.users = data;
       $scope.currentUser = UserService.currentUser;
-      // getUsers.resolve(data);
 
+      // define function that calculates width percentage for your bar chart
       $scope.calculateYourWidth = function (user, activity){
-
-        if ($scope.currentUser.stats[activity] - user[activity] >= 0)
+        // if you have more steps than the user on the card
+        if ($scope.currentUser.stats[activity] - user[activity] >= 0){
+          // your bar will be width 100%
           return {width: "100%"};
-        else {
+        } else {
+          // your bar will be a normalized percentage of their bar chart width
           return {width: String(~~(100*($scope.currentUser.stats[activity]/user[activity]))) + "%"};
         }
       };
 
+      // define function calculates width percentage for opponent's bar chart
+      // it's the reverse from the function above...
       $scope.calculateFriendWidth = function (user, activity){
-
         if (user[activity] - $scope.currentUser.stats[activity] >= 0)
           return {width: "100%"};
         else {
@@ -33,9 +39,12 @@ angular.module('fittr.controllers')
         }
       };
 
+    });
+
   });
 
 
+    
 
  // Response object for UserService.getAll
  // -----------------------------------------------------
