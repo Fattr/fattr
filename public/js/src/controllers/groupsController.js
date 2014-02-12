8ca1b52246a286aad936bd35cc3d370d1059551f
sudostack <dwu207@gmail.com>
2014-02-12 10:35:46 -0800

    outline for how groups will function

--- public/js/src/controllers/groupsController.js
+++ public/js/src/controllers/groupsController.js
@@ -1,12 +1,39 @@
 angular.module('fittr.controllers')
 
-.controller('GroupsController', ['$scope', 'UserService', function($scope, UserService) {
+.controller('GroupsController',
+  [
+    '$scope',
+    'UserService',
+    'GroupService',
+    function($scope, UserService, GroupService) {
 
-  $scope.user = {};
+    // Current user 
+    var user = UserService.currentUser;
 
-  $scope.user.groups = [
-    'Hack Reactor',
-    '2013-11'
-  ];
+    /*
+     * Group creation
+     */
+    $scope.createGroup = function(name) {
+      GroupService.create(name, user);
+    };
+
+    /*
+     * Fetch Groups
+     */ 
+
+    // In Groups
+    $scope.user.fetchGroups = function() {
+      GroupService.fetch(user);
+    };
+
+    // Available Groups
+    $scope.groupsAvail = function() {
+      GroupService.available();
+    };
+
+    // Search Groups
+    $scope.search = function(params) {
+      GroupService.findGroup();
+    };
 
 }]);
