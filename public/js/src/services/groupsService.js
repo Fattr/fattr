aa825601220d501c47e9367405e15b4855ad2dcb
sudostack <dwu207@gmail.com>
2014-02-12 10:35:46 -0800

    outline for how groups will function

--- public/js/src/services/groupsService.js
+++ public/js/src/services/groupsService.js
@@ -3,10 +3,40 @@
  *
  * This service's primary job is to add/remove user(s)
  * from group(s)
+ * 
+ * It also permits users to create groups
  */
  
-angular.module('fittr.services').
+angular.module('fittr.services')
 
-provider('GroupService', function() {
+.provider('GroupService', function() {
+  this.$get = function($http, $q) {
+    return {
+
+      // http helper since http operations are repeated
+      // several times within this service.
+      _httpHelper: function(verb, url, body) {
+        var d = $q.defer();
+        $http[verb](url, body)
+          .success(function(data, status, headers, config) {
+            d.resolve(data);
+          })
+          .error(function(data, status, headers, config) {
+            d.reject(data, status);
+          });
+        return d.promise;
+      },
+
+      // Create group
+      create: function(name) {
+        return this._httpHelper('post', '/group', name);
+      },
+
+      // Fetch group
+      fetch: function(name) {
+        return this._httpHelper('get', '/group', name);
+      }
+    }
+  }
 
 });
