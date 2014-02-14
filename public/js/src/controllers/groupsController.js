19b78d2cd5e8b1dcda3f47bf6c8cfacb709d3fd8
sudostack <dwu207@gmail.com>
2014-02-13 19:59:29 -0800

    preloading groups view

--- public/js/src/controllers/groupsController.js
+++ public/js/src/controllers/groupsController.js
@@ -6,37 +6,37 @@ angular.module('fittr.controllers')
   'GroupService',
   function($scope, UserService, GroupService) {
 
-  // Current user 
-  var user = UserService.currentUser;
-
-  $scope.notInGroup = false;
-
-  // GROUPS
-
-  // Current View
-  $scope.view = function(view) {
-    if (view === 'in') {
-      $scope.v = 'in';
-      $scope.groups = GroupService.inGroups();
-      console.log($scope.groups);
-    } else if (view === 'find') {
-      $scope.v = 'find';
+    // Current View
+    $scope.view = function(view) {
+      if (view === 'in') {
+        $scope.v = 'in';
+        $scope.groups = GroupService.inGroups();
+      } else if (view === 'find') {
+        $scope.v = 'find';
+        // temp until endpoint is set up on server
+        $scope.groups = [
+          {name: 'found group 1'},
+          {name: 'found group 2'},
+          {name: 'found group 3'}
+        ]
+      }
     }
-  }
 
-  /*
-   * Create Groups
-   */
-  $scope.createGroup = function(name) {
-    return GroupService.create(name);
-  };
+    /*
+     * Create Groups
+     */
+    $scope.createGroup = function(name) {
+      return GroupService.create(name);
+    };
 
-  /*
-   * Fetch Groups
-   */
+    /*
+     * Fetch Groups
+     */
 
-  $scope.addMe = function(groupName) {
-    GroupService.addToGroup(groupName);
-  };
+    $scope.addMe = function(groupName) {
+      GroupService.addToGroup(groupName);
+    };
+
+    $scope.view('in');
 
 }]);
